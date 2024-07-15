16acc3510f6eaae45b70d905c742e750e8683fb6
Akuev Alexandr <akuev.sasha@mail.ru>
2024-07-15 15:06:11 +0300

    Настроил DbContext, теперь буду настраивать миграции. Точка для отката, если неправильно сделаю миграцию

---
+++ Messanger.Persistence/MessengerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Messenger.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Messenger.Persistence;
+
+public class MessengerDbContext(DbContextOptions<MessengerDbContext> options) 
+    : DbContext(options)
+{
+    public DbSet<ChatEntity>? Chats { get; set; }
+    public DbSet<ContactEntity>? Contacts { get; set; }
+    public DbSet<GroupEntity>? Groups { get; set; }
+    public DbSet<MessageEntity>? Messages { get; set; }
+    public DbSet<UserEntity>? Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
--- Messanger/Program.cs
+++ Messanger/Program.cs
@@ -1,4 +1,14 @@
+using Messenger.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MessengerDbContext>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(MessengerDbContext)));
+    });
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
